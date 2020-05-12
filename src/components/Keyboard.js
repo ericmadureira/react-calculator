95e8ffead704f7e3f7e84e8b13adc28e2c4fa181
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-05-12 17:48:52 -0300

    fet: use Key component in Keyboard
    Memoize Key buttons.

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import { keyboard } from '../constants';
+import React, { useMemo } from 'react';
+
+import { calcKeys } from '../constants';
+import Key from './Key';
 
 const Keyboard = () => {
+  const keyboard = useMemo(() => (
+    calcKeys.map(key => <Key label={key} />)
+  ), []);
+
   return (
     <div className='keyboard'>
       {keyboard}
