aba25c8edaa5c57243b27f590f6513721c04190e
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-05-12 19:43:32 -0300

    feat: styled component Calculator
    Changes Calculator to styled component.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
 import Keyboard from './Keyboard'
 import Display from './Display';
 
-const Calculator = () => {
+const Calculator = styled.div`
+  align-items: center;
+  border: 1px black solid;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  width: 250px;
+`;
+
+const Container = () => {
   const [result, setResult] = useState('');
 
   return (
-    <div>
+    <Calculator>
       <Display
         result={result}
         setResult={setResult}
@@ -15,8 +26,8 @@ const Calculator = () => {
         result={result}
         setResult={setResult}
       />
-    </div>
+    </Calculator>
   );
 };
 
-export default Calculator;
+export default Container;
