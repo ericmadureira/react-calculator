5574e95d2dc10fb018ba58c9cd61f3cc27026cdd
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-05-12 19:44:19 -0300

    feat: styled Display and update logic
    Changes to styled component and adds update logic.

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
 
-const Display = () => (
-  <input type='text'/>
-);
+const Display = styled.input`
+  font-size: 2em;
+  margin-bottom: 10px;
+  padding: 4px;
+`;
 
-export default Display;
+const Container = ({ result, setResult }) => {
+  const updateDisplay = useCallback(
+    e => setResult(result + e.key),
+    [result, setResult],
+  );
+
+  return (
+    <Display
+      type='text'
+      value={result}
+      onKeyPress={updateDisplay}
+    />
+  )
+};
+
+export default Container;
